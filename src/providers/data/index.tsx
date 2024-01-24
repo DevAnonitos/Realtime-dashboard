50e27d27ea74240d3fb0e3d238979d243de6412c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-24 22:31:19 +0700

    update:Add->Provider-AppPage

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,8 +1,16 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, { 
+  GraphQLClient,
+  liveProvider as graphqlLiveProvider,
+} from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient  } from "graphql-ws";
 
+
+export const API_BASE_URL = "https://api.crm.refine.dev";
 export const API_URL = "https://api.crm.refine.dev";
+export const WS_URL = "wss://api.crm.refine.dev";
 
+// Setting up client 
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
     try {
@@ -12,3 +20,19 @@ export const client = new GraphQLClient(API_URL, {
     }
   }
 });
+
+export const wsClient = typeof window !== "undefined" ? createClient({
+  url: WS_URL,
+  connectionParams: () => {
+    const accessToken = localStorage.getItem("access_token");
+
+    return {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      }
+    }
+  }
+}) : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined;
