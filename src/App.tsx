8a987104f4c0f522b54427cfdcacf2345b4cd575
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-25 13:09:13 +0700

    update:Add->ConfigRoute

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,9 @@ function App() {
                   <Route path="/register" element={<Register />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route>
+                    
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
