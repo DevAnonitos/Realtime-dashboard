0b7ec90c7f49561de8a5ffb81aa6ae30ce6cfcbf
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-25 12:35:47 +0700

    update:Add->ConfigFooterLayout

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ThemedLayoutV2, ThemedTitleV2 } from '@refinedev/antd';
 import Header from './Header';
+import Footer from './footer';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <>
       <ThemedLayoutV2
         Header={Header}
+        Footer={Footer}
         Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
       >
         {children}
