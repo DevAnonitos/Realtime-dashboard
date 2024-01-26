8607816c301fabe829bde9b153cca9e2255e5d34
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-26 09:15:16 +0700

    update:Add->ConfigCurrentUser

--- src/components/layouts/current-user.tsx
+++ src/components/layouts/current-user.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import { Popover, Button } from 'antd';
+import { SettingOutlined } from "@ant-design/icons";
 
 const CurrentUser = () => {
+
+  const content = (
+    <>
+      <div 
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            borderTop: "1px solid #d9d9d9",
+            padding: "4px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "4px",
+          }}
+        >
+          <Button
+            style={{ textAlign: "left" }}
+            icon={<SettingOutlined />}
+            type="text"
+            block
+            // onClick={() => {}}
+          >
+            Account settings
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+
   return (
     <>
       <Popover 
         placement='bottomRight'  
         trigger="click" 
+        content={content}
         overlayInnerStyle={{ padding: 0 }} 
         overlayStyle={{ zIndex: 999 }}
       >
