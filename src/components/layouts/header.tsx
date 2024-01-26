6720123e20bf4762a7e466abda1a929cd4229a7a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-26 09:49:57 +0700

    update:Add->Layout-Header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import CurrentUser from './current-user';
+import { Layout, Space, theme } from 'antd';
+
+const { useToken } = theme;
 
 const Header = () => {
+
+  const { token } = useToken();
+
+  const headerStyle: React.CSSProperties = {
+    backgroundColor: token.colorBgElevated,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0px 24px",
+    position: "sticky",
+    top: "0px",
+    zIndex: 999,
+  };
+
   return (
-    <div>
-      <CurrentUser />
-    </div>
+    <>
+      <Layout.Header style={headerStyle}>
+        <Space align='center' size="middle">
+          <CurrentUser />
+        </Space>
+      </Layout.Header>
+    </>
   );
 };
 
--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ThemedLayoutV2, ThemedTitleV2 } from '@refinedev/antd';
 import Header from './header';
+import Footer from './footer';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <>
       <ThemedLayoutV2
         Header={Header}
+        Footer={Footer}
         Title={(titleProps) => {
           return <ThemedTitleV2 {...titleProps} text="Refine" />;
         }}
