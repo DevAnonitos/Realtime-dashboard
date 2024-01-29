51428a6f90d6bc8d1e97813b8bef2a4f52c58809
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-30 01:01:27 +0700

    update:Add->ConfigPath

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ["antd"],
+        },
+      },
+    },
+  },
 });
